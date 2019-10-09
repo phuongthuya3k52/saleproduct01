0e9ea92edcfa86cdcf3e9a46a13978278e6ede38
Nguyen Phuong Thuy <phuongthuya3k52@gmail.com>
2019-10-09 15:38:39 +0700

    add new product file

---
+++ createProduct.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Create Product</title>
+</head>
+<body>
+<form method="post" action="doCreateProduct">
+	<table>
+		<tr>
+			<td>Name</td>
+			<td><input type="text" name="txtName"></td>
+		</tr>
+		<tr>
+			<td>Price</td>
+			<td><input type="text" name="txtPrice"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" name="ADD"></td>
+
+		</tr>
+	</table>
+
+</form>
+</body>
+</html>
---
+++ doCreateProduct.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Processing</title>
+</head>
+<body>
+	<?php 
+		$name = $_POST["txtName"];
+		$price = $_POST["txtPrice"];	
+		echo $name;
+		
+		//Refere to database 
+	   $db = parse_url(getenv("DATABASE_URL"));
+	   $pdo = new PDO("pgsql:" . sprintf(
+	        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+	        $db["host"],
+	        $db["port"],
+	        $db["user"],
+	        $db["pass"],
+	        ltrim($db["path"], "/")
+	   ));
+	   $data = [
+		    'name' => $name,
+		    'price' => $price,
+		];
+		$stmt =  $pdo->prepare("INSERT INTO product(name, price) VALUES (:name,:price)");	
+		$stmt->execute($data);
+
+	 ?>
+	 <h2>You have added <?php echo $name?>  product sucessfully!</h2>
+	<!--  <ul>
+	 	<li><?php echo $birthday?></li>
+	 	<li><?php echo $gender?></li>
+	 	<li><?php echo $fav_book?></li>
+	 	<li><?php echo $fav_car?></li>
+	 </ul> -->
+	 <a href="index.php">Index</a>
+</body>
+</html>
